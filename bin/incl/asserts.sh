8e65f54c0086b288b46976a6c0f9f6f231830847
hb <henrik.bengtsson@gmail.com>
2024-11-25 16:49:34 -0800

    CLEANUP: moved more assert functions to incl/asserts.sh

--- bin/incl/asserts.sh
+++ bin/incl/asserts.sh
@@ -47,3 +47,76 @@ function assert_executable {
 function assert_integer {
     is_integer "${1:?}" || error "Not an integer: ${1}"
 }
+
+
+#' Assert that all folders specified in a "path" environment variable exist
+#'
+#' Usage: assert_envpath <envvar> [exclude]*
+#'
+#' Examples:
+#' assert_envpath LD_LIBRARY_PATH
+#'
+#' # Same, but ignore anything under HOME
+#' assert_envpath LD_LIBRARY_PATH "$HOME"
+assert_envpath_exist() {
+    local name=${1:?}
+    local path=${!name}    
+    local excl
+    local -a missing
+
+    ## Nothing to do?
+    [[ -z ${path} ]] && return 0
+
+    shift
+    excl=$(IFS="|"; echo "$*")
+    
+    IFS=':' read -r -a dirs <<< "$path"
+    for dir in "${dirs[@]}"; do
+	[[ -z ${dir} ]] && continue
+	[[ -n ${excl} ]] && grep -q -E "${excl}" <<< "$dir" && continue
+        [[ -d "${dir}" ]] || { missing+=( "$dir" ); continue; }
+        [[ -r "${dir}" ]] || { missing+=( "$dir [no permission]" ); continue; }
+        [[ -x "${dir}" ]] || { missing+=( "$dir [no recursive permission]" ); continue; }
+    done
+    
+    if [[ ${#missing[@]} -gt 0 ]]; then
+	 >&2 echo "Detected non-existing or inaccessible folder(s) in ${name}: [n=${#missing[@]}] ${missing[*]}"
+	return 1
+    fi
+    
+    return 0
+}
+
+
+assert_MODULEPATH_exist() {
+    ## FIXME: Do not add those missing folders in the first place /HB 2023-10-04
+    assert_envpath_exist MODULEPATH /usr/share/modulefiles/Linux /usr/share/modulefiles/Core
+}
+
+assert_PATH_exist() {
+    assert_envpath_exist PATH "$HOME" "$@"
+}
+
+assert_LD_LIBRARY_PATH_exist() {
+    assert_envpath_exist LD_LIBRARY_PATH "$HOME" "$@"
+}
+
+assert_MANPATH_exist() {
+    assert_envpath_exist MANPATH "$HOME" "$@"
+}
+
+assert_INFO_PATH_exist() {
+    assert_envpath_exist INFO_PATH "$HOME" "$@"
+}
+
+assert_CPATH_exist() {
+    assert_envpath_exist CPATH "$HOME" "$@"
+}
+
+assert_PKG_CONFIG_PATH_exist() {
+    assert_envpath_exist PKG_CONFIG_PATH "$HOME" "$@"
+}
+
+assert_CUDA_LIB_PATH_exist() {
+    assert_envpath_exist CUDA_LIB_PATH "$HOME" "$@"
+}
--- bin/incl/ldap.sh
+++ bin/incl/ldap.sh
@@ -1,96 +1,6 @@
 #!/usr/bin/env bash
 
 # -------------------------------------------------------
-# ASSERTS
-# -------------------------------------------------------
-#' Assert that all folders specified in a "path" environment variable exist
-#'
-#' Usage: assert_envpath <envvar> [exclude]*
-#'
-#' Examples:
-#' assert_envpath LD_LIBRARY_PATH
-#'
-#' # Same, but ignore anything under HOME
-#' assert_envpath LD_LIBRARY_PATH "$HOME"
-assert_envpath_exist() {
-    local name=${1:?}
-    local path=${!name}    
-    local excl
-    local -a missing
-
-    ## Nothing to do?
-    [[ -z ${path} ]] && return 0
-
-    shift
-    excl=$(IFS="|"; echo "$*")
-    
-    IFS=':' read -r -a dirs <<< "$path"
-    for dir in "${dirs[@]}"; do
-	[[ -z ${dir} ]] && continue
-	[[ -n ${excl} ]] && grep -q -E "${excl}" <<< "$dir" && continue
-        [[ -d "${dir}" ]] || { missing+=( "$dir" ); continue; }
-        [[ -r "${dir}" ]] || { missing+=( "$dir [no permission]" ); continue; }
-        [[ -x "${dir}" ]] || { missing+=( "$dir [no recursive permission]" ); continue; }
-    done
-    
-    if [[ ${#missing[@]} -gt 0 ]]; then
-	 >&2 echo "Detected non-existing or inaccessible folder(s) in ${name}: [n=${#missing[@]}] ${missing[*]}"
-	return 1
-    fi
-    
-    return 0
-}
-
-
-assert_MODULEPATH_exist() {
-    ## FIXME: Do not add those missing folders in the first place /HB 2023-10-04
-    assert_envpath_exist MODULEPATH /usr/share/modulefiles/Linux /usr/share/modulefiles/Core
-}
-
-assert_PATH_exist() {
-    assert_envpath_exist PATH "$HOME" "$@"
-}
-
-assert_LD_LIBRARY_PATH_exist() {
-    assert_envpath_exist LD_LIBRARY_PATH "$HOME" "$@"
-}
-
-assert_MANPATH_exist() {
-    assert_envpath_exist MANPATH "$HOME" "$@"
-}
-
-assert_INFO_PATH_exist() {
-    assert_envpath_exist INFO_PATH "$HOME" "$@"
-}
-
-assert_CPATH_exist() {
-    assert_envpath_exist CPATH "$HOME" "$@"
-}
-
-assert_PKG_CONFIG_PATH_exist() {
-    assert_envpath_exist PKG_CONFIG_PATH "$HOME" "$@"
-}
-
-assert_CUDA_LIB_PATH_exist() {
-    assert_envpath_exist CUDA_LIB_PATH "$HOME" "$@"
-}
-
-
-assert_no_base64() {
-    local field
-    local value=${1:?}
-    local pattern="^([[:alpha:]]+):: ([A-Za-z0-9]+[=*])$"
-    if grep -i -E "${pattern}" <<< "${value}"; then
-        field=$(sed -E "s/${pattern}/\1/" <<< "${value}")
-        value=$(sed -E "s/${pattern}/\2/" <<< "${value}")
-	>&2 echo "ERROR: Detected a Base64 encoded value in field '${field}': ${value} => '$(base64 --decode <<< "${value}")'"
-	exit 1
-    fi
-}    
-
-
-
-# -------------------------------------------------------
 # Regular expressions
 # -------------------------------------------------------
 email_pattern() {
@@ -149,6 +59,19 @@ reserved_usernames() {
 LDAP_URI="ldap://m1,ldap://m2"
 LDAP_SEARCHBASE="dc=cgl,dc=ucsf,dc=edu"
 LDAP_FILTER="(wyntonAccess=TRUE)(!(wyntonAdmin=TRUE))"
+
+assert_no_base64() {
+    local field
+    local value=${1:?}
+    local pattern="^([[:alpha:]]+):: ([A-Za-z0-9]+[=*])$"
+    if grep -i -E "${pattern}" <<< "${value}"; then
+        field=$(sed -E "s/${pattern}/\1/" <<< "${value}")
+        value=$(sed -E "s/${pattern}/\2/" <<< "${value}")
+	>&2 echo "ERROR: Detected a Base64 encoded value in field '${field}': ${value} => '$(base64 --decode <<< "${value}")'"
+	exit 1
+    fi
+}    
+
 
 ldap_search_raw() {
     ldapsearch "$@"
